4fbe9222fd79ee31b7ec031b0be070a9a400d1d3
Frank Richter <frank.richter@gmail.com>
2017-01-08 15:13:46 +0100

    wrestool: Make check_offset more stringent

--- wrestool/fileread.c
+++ wrestool/fileread.c
@@ -35,12 +35,15 @@
 bool
 check_offset(char *memory, int total_size, char *name, void *offset, int size)
 {
-	ptrdiff_t need_size = (char *) offset - memory + size;
+	char* memory_end = memory + total_size;
+	char* block = (char*)offset;
+	char* block_end = offset + size;
 
 	/*debug("check_offset: size=%x vs %x offset=%x size=%x\n",
 		need_size, total_size, (char *) offset - memory, size);*/
 
-	if (need_size < 0 || need_size > total_size) {
+	if (((memory <= memory_end) && (block <= block_end))
+		&& ((block < memory) || (block >= memory_end) || (block_end > memory_end))) {
 		warn(_("%s: premature end"), name);
 		return false;
 	}
