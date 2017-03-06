bfc9bf725ccd04c2fa54f4f74c9d06a1ac5699b9
Frank Richter <frank.richter@gmail.com>
2017-03-06 21:04:48 +0100

    wrestool: Fix check_offset to always accept pointer if block or total size is negative. Duh.

--- wrestool/fileread.c
+++ wrestool/fileread.c
@@ -42,8 +42,8 @@ check_offset(char *memory, int total_size, char *name, void *offset, int size)
 	/*debug("check_offset: size=%x vs %x offset=%x size=%x\n",
 		need_size, total_size, (char *) offset - memory, size);*/
 
-	if (((memory <= memory_end) && (block <= block_end))
-		&& ((block < memory) || (block >= memory_end) || (block_end > memory_end))) {
+	if (((memory > memory_end) || (block > block_end))
+		|| (block < memory) || (block >= memory_end) || (block_end > memory_end)) {
 		warn(_("%s: premature end"), name);
 		return false;
 	}
